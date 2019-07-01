b84be90fc773944101fe744cc2fbeac52fcd7614
Luis Fraga <abdi@live.jp>
2019-07-02 07:56:16 +0900

    Update and rename MAF_and_CisMarking.R to 2_CisNeighborhood.R

--- ExperimentCode/2_CisNeighborhood.R
+++ ExperimentCode/2_CisNeighborhood.R
@@ -7,38 +7,14 @@ snp_inf <- read.table("/home/lffraga/Table Data/snp_inf21.txt",header = TRUE)
 gene <- read.table("/home/lffraga/Raw Data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt",header = TRUE)
 
 ### Gene expression match
-start<-Sys.time()
-
-#select chromosome in gene expression dataset
+### Select chromosome in gene expression dataset
 gene_loc <- gene[grep("^21$", gene[,3]),]
 geneLocation <- gene_loc[,4]
 snps <- t(snp_inf)
 
-#t <- gene_expression_chr[,5:466] # 462 samples
-#match_sample_g <- match(rownames(snps),names(gene_expression_chr[,5:466]))
-#match_sample_g <- na.omit(match_sample_g)
-#length(match_sample_g)
-
-#expr <- data.frame(gene_expression_chr[,1], t[,match_sample_g])
-#dim(expr)
-#expr[1:5,1:5]
-
-#gene_ex<-expr[-1] #delete first column
-#gene_ex[1:5,1:5]
-#dim(gene_ex)
-
-
-#gene_loc <- data.frame(gene_expression_chr[,1],gene_expression_chr[,3:4])
-#dim(gene_loc)
-#row.names(gene_loc)<-NULL
-
-end<-Sys.time()
-end-start
-
 ### Cis-SNPs marking
 start<-Sys.time()
 
-#geneLocation <- gene_loc[,3]
 cis_matrix <- list() 
 tmp <- list()
 
@@ -60,7 +36,8 @@ end<-Sys.time()
 end-start
 
 ################################################ FINAL PART START ################################################
-#names(cis_matrix[!is.na(cis_matrix[,1]),1])
+start<-Sys.time()
+
 l <- list()
 c <- list()
 
@@ -80,13 +57,8 @@ for(i in 1:dim(cis_matrix)[2])
   c <- cbind.fill(c, names(cis_matrix[!is.na(cis_matrix[,i]),i]), fill = NA) ## errado aqui
 }
 
-#c <- c[,-1]
-#colnames(c) <- colnames(BH_passed_snp)
-#dim(c)
-#dim(c[,-1])
-#dim(cis_matrix)
-#c[1:5,1:5]
-#cis_matrix[1:5,1:5]
+end<-Sys.time()
+end-start
 ################################################ FINAL PART END ################################################
 
 ### Saving data
